1838ac4d8e52f087d09ff1331b2c62e557c9243c
Harry Xu <harryxu2626@gmail.com>
2024-11-08 20:00:06 -0500

    Updates

--- _next/static/bDxb1KDE__LQI43THzyty/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/50-2591c9d350331ef7.js","static/chunks/pages/index-133a2e421ed46043.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
