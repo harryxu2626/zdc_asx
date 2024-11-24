d60dcb0937eb27e091147602da98b7f8423444b1
Harry Xu <harryxu2626@gmail.com>
2024-11-24 15:44:34 -0500

    Updates

--- _next/static/OSM2HxCblBO-jKQZBXs4V/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/170-89cff7465a7211ea.js","static/chunks/pages/index-5b76f76c9ba77bf2.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/rC0eqPt1P58xM6fycB2jC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/170-89cff7465a7211ea.js","static/chunks/pages/index-daeb37caf3ca697e.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
